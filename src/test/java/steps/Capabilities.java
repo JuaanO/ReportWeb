4fb1104fe3e700b9d681b6114c67694ebdb39d04
Juan Jose <you@example.com>
2021-10-04 13:15:23 -0500

    Renaming methods

--- src/test/java/steps/Capabilities.java
+++ src/test/java/steps/Capabilities.java
@@ -11,19 +11,19 @@ public class Capabilities extends TestBase {
 
     private static int TIMEOUT = 30;
 
-    public static Properties propo() throws IOException{
-        Properties props = new Properties();
-        props.load(new FileReader("src/test/resources/config.properties"));
-        return props;
+    public static Properties properties() throws IOException{
+        Properties properties = new Properties();
+        properties.load(new FileReader("src/test/resources/config.properties"));
+        return properties;
     }
 
     public static void initDocker(WebDriver driver) throws IOException {
         System.out.println("-------> Docker Execution..!!");
         driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(propo().getProperty("production"));
+        driver.get(properties().getProperty("production"));
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(propo().getProperty("user"), propo().getProperty("password"));
+        loginPage.login(properties().getProperty("user"), properties().getProperty("password"));
         loginPage.assertionLogin();
     }
 
@@ -31,9 +31,9 @@ public class Capabilities extends TestBase {
         System.out.println("-------> Local Execution..!!");
         driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(propo().getProperty("production"));
+        driver.get(properties().getProperty("production"));
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(propo().getProperty("user"), propo().getProperty("password"));
+        loginPage.login(properties().getProperty("user"), properties().getProperty("password"));
         loginPage.assertionLogin();
     }
 }
